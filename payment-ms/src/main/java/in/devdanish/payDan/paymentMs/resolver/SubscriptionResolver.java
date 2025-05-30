14a1c4a68b449ee64dd5911c31dcf9a42ebb3bbc
Danish Javed <danishjaved636@gmail.com>
2025-05-30 10:39:05 +0530

    Adds graphql subscriptions

--- payment-ms/src/main/java/in/devdanish/payDan/paymentMs/resolver/SubscriptionResolver.java
+++ payment-ms/src/main/java/in/devdanish/payDan/paymentMs/resolver/SubscriptionResolver.java
@@ -3,6 +3,7 @@ package in.devdanish.payDan.paymentMs.resolver;
 import in.devdanish.payDan.paymentMs.model.Transaction;
 import in.devdanish.payDan.paymentMs.publisher.TransactionPublisher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
@@ -13,7 +14,7 @@ public class SubscriptionResolver {
     TransactionPublisher transactionPublisher;
 
     @SubscriptionMapping
-    public Flux<Transaction> onTransactionAdded(String walletId){
+    public Flux<Transaction> onTransactionAdded(@Argument String walletId){
         return transactionPublisher.subscribe(walletId);
     }
 }
